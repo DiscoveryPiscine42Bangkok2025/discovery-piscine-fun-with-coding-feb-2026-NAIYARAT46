07ebf65dd8fa9ee45c0948ac711762a896012012
JN <65010477@kmitl.ac.th>
2026-02-07 14:17:29 +0700

    Update balloon.js

--- cell03/ex04/ex00bis/balloon.js
+++ cell03/ex04/ex00bis/balloon.js
@@ -1,21 +1,21 @@
 $(function () {
   let size = 200;
   let colors = ["red", "green", "blue"];
-  let index = 0;
+  let i = 0;
 
   $("#balloon").click(function () {
     size += 10;
-    index = (index + 1) % colors.length;
+    i = (i + 1) % colors.length;
 
     if (size > 420) {
       size = 200;
-      index = 0;
+      i = 0;
     }
 
     $(this).css({
       width: size + "px",
       height: size + "px",
-      background: colors[index]
+      background: colors[i]
     });
   });
 
@@ -23,12 +23,12 @@ $(function () {
     size -= 5;
     if (size < 200) size = 200;
 
-    index = (index - 1 + colors.length) % colors.length;
+    i = (i - 1 + colors.length) % colors.length;
 
     $(this).css({
       width: size + "px",
       height: size + "px",
-      background: colors[index]
+      background: colors[i]
     });
   });
 });
